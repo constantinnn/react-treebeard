7c305be76da0f14f144282af8e62715a8a964b25
constantinnn <constantin.dan21@gmail.com>
2018-07-04 14:20:12 +0300

    Update animations.js

--- src/themes/animations.js
+++ src/themes/animations.js
@@ -3,16 +3,16 @@
 export default {
     toggle: ({node: {toggled}}) => ({
         animation: {rotateZ: toggled ? 90 : 0},
-        duration: 300
+        duration: 100
     }),
     drawer: (/* props */) => ({
         enter: {
             animation: 'slideDown',
-            duration: 300
+            duration: 100
         },
         leave: {
             animation: 'slideUp',
-            duration: 300
+            duration: 100
         }
     })
 };
